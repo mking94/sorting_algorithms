4250c2ba96ebab51a1e5b452cd4f8e99aa08de1a
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-10-24 16:22:05 +0100

    Function of bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,7 +7,7 @@
 
 void bubble_sort(int *array, size_t size)
 {
-int swap = 1, passage = 0;
+int swap = 1, passage = 0, temp;
 size_t i;
 
 if(array == NULL || size < 2)
@@ -19,9 +19,9 @@ for(i = 0; i < size - passage; i++)
 if(array[i] > array[i + 1])
 {
 swap = 1;
-array[i] = array[i] + array[i + 1];
-array[i + 1] = array[i] - array[i + 1];
-array[i] = array[i] - array[i + 1];
+temp = array[i];
+array[i] = array[i + 1];
+array[i + 1] = temp;
 print_array(array, size);
 }
 }
