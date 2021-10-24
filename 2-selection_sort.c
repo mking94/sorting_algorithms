2325e432d6cca1b4941987f043972e74e7f084a7
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-10-24 16:11:42 +0100

    Function of selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * selection_sort - function sorts an array in ascending using the selection sort.
+ * selection_sort - function sorts an array using the selection sort.
  * @array: input array.
  * @size: size of array.
  *
@@ -11,13 +11,13 @@ void selection_sort(int *array, size_t size)
 {
 size_t i, j, min, temp;
 
-if(array == NULL || size < 2)
+if (array == NULL || size < 2)
 return;
-for(i = 0 ; i < size - 1 ; i++)
+for (i = 0 ; i < size - 1 ; i++)
 {
 min = i;
-for(j = i + 1 ; j < size ; j++)
-if(array[j] < array[min])
+for (j = i + 1 ; j < size ; j++)
+if (array[j] < array[min])
 min = j;
 if(min != i)
 {
