9e7efe7a4e2e48285b590c09bbe94b1b852aaa8e
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-10-24 18:45:17 +0100

    Function of quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * quick_sort - function sorts an array using the quick sort.
+ * @array: table of array
+ * @size: size of array.
+ */
+
+void quick_sort(int *array, size_t size)
+{
+size_t mur, courant
+int pivot, tmp;
+
+if (size < 2 || array == NULL)
+return;
+pivot = array[size - 1];
+mur  = courant = 0;
+while (courant < size)
+{
+if (array[courant] <= pivot)
+{
+if (mur != courant)
+{
+tmp = array[courant];
+array[courant] = array[mur];
+array[mur] = tmp;              
+}
+mur++;
+}
+courant++;
+}
+quick_sort(array, mur - 1);
+quick_sort(arra + mur - 1, taille - mur + 1);
+}
