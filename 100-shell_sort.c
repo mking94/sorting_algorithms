a00d9c17089485a7c5d8a206aa13a933ff55ccc5
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-10-25 13:45:01 +0100

    Function of shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,18 +7,26 @@
  */
 void shell_sort(int *array, size_t size)
 {
-for (size_t gap = size / 2; gap > 0; gap /= 2)
-{
-for (size_t i = gap; i < size; i += 1)
-{
-int temp = array[i];
-size_t j;
-for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-{
-array[j] = array[j - gap];
-}
-array[j] = temp;
-print_array(array, size);
-}
-}
+	size_t i = 0, gap = 1, j;
+	int temp;
+
+	if (!array || size <= 1)
+		return;
+
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+	while (gap >= 1)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = temp;
+		}
+		print_array(array, size);
+		gap /= 3;
+	}
 }
